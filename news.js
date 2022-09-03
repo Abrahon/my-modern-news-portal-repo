435fdcba48680eac94626cfae8a1235cde5e9b2b
Abrahon <90842196+Abrahon@users.noreply.github.com>
2022-09-04 01:50:26 +0600

    added news counter

--- news.js
+++ news.js
@@ -1,4 +1,5 @@
 const loadNewses = () => {
+    spinner(true)
     fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
         .then(data => displayCategories(data.data.news_category))
@@ -28,6 +29,8 @@ const displayCategories = categories => {
 }
 
 loadCategoryNews = (id) => {
+    spinner(true)
+
     // console.log(id)
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
     console.log(id)
@@ -40,6 +43,7 @@ loadCategoryNews = (id) => {
 
 const displayNewsDetailes = details => {
     // console.log(news);
+    displayNewsCount(details.length)
     const newsDetail = document.getElementById('news-detail');
     newsDetail.innerHTML = ''
     details.forEach(detail => {
@@ -54,7 +58,7 @@ const displayNewsDetailes = details => {
                     <div class="col-md-8">
                     <div class="card-body">
                     <h4 class="card-title">${detail.title}</h4>
-                    <p class="card-text my-3">${detail.details.slice(0, 800)}...</p>
+                    <p class="card-text my-3">${detail.details.slice(0, 700)}...</p>
                     <div class="row">
                     <div class="col-md-4 d-flex justify-content-between"">
                         <img class="w-25 rounded-circle me-3" src="${detail.author.img}" alt="">
@@ -65,11 +69,12 @@ const displayNewsDetailes = details => {
                        
                     </div>
                     <div class="col-md-4">
-                        <p></p>
-                        <h4> ${detail.total_view ? detail.total_view : '00'} M</h4>
+                        <h4> ${detail.total_view ? detail.total_view : '00'}K</h4>
                     </div>
                     <div class="col-md-4">
-                        <h4 class='btn btn-primary'>Details</h4>
+                    <button onclick="loadData()" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Launch demo modal
+                  </button>
                     </div>
 
                 </div>
@@ -81,26 +86,31 @@ const displayNewsDetailes = details => {
 
 
     })
+
+
     spinner(false)
 }
 //    spenir showing
-document.getElementById("news-detail").innerHTML = addEventListener("click", function () {
-    //     // start loding
-    spinner(true)
 
-})
 // // spinner function
-const spinner = isLoading => {
+function spinner(isLoading) {
     const loderSection = document.getElementById("loader");
 
 
-    if (isLoading === true) {
+    if (isLoading) {
         loderSection.classList.remove("d-none");
     }
     else {
         loderSection.classList.add("d-none");
     }
 }
+const displayNewsCount = (newslength) => {
+    const newsLenthCounter = document.getElementById('newslength-container');
+    newsLenthCounter.innerText = newslength;
+}
+
+loadCategoryNews('08')
+
 
 
 loadNewses();
